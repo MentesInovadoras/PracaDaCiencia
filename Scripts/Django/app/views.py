a3371d6be2513ff1b27e5f80d160e8e359685eef
MentesInovadoras <166884223+MentesInovadoras@users.noreply.github.com>
2024-09-20 15:11:29 -0300

    Delete Scripts directory

--- Scripts/Django/app/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render
-
-def main(request):
-    return render(request, "Default.html")
-# Create your views here.
--- Scripts/__init__.py
+++
@@ -1,7 +0,0 @@
-import dotenv
-import os
-
-
-dotenv.load_dotenv()
-EMAIL: str = os.getenv('EMAIL')
-EMAIL_PASSWORD: str = os.getenv('EMAIL_PASSWORD')
