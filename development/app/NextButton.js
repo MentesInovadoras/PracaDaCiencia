cad3bcec33b6d324aae272794b0cbfc946545f1b
Caio Coronel <caiocesarcoronel@gmail.com>
2024-09-25 14:49:15 -0300

    build(PRACA): arrumei as coisa

--- development/app/NextButton.js
+++
@@ -1,7 +0,0 @@
-export default function NextButton(){
-    return(
-        <button id="next_button" className="next_button">
-          &gt;
-        </button>
-    )
-}
--- development/app/PrevButton.js
+++
@@ -1,7 +0,0 @@
-export default function PrevButton(){
-    return(
-        <button id="prev_button" className="prev_button">
-          &lt;
-        </button>
-    )
-}
