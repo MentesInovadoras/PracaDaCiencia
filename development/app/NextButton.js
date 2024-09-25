bd324cfdc18746e0af4ab5457fee9448e1bb0fbd
CCCoronel <160687497+CCCoronel@users.noreply.github.com>
2024-09-25 14:05:50 -0300

    Add files via upload

---
+++ development/app/NextButton.js
@@ -0,0 +1,7 @@
+export default function NextButton(){
+    return(
+        <button id="next_button" className="next_button">
+          &gt;
+        </button>
+    )
+}
---
+++ development/app/PrevButton.js
@@ -0,0 +1,7 @@
+export default function PrevButton(){
+    return(
+        <button id="prev_button" className="prev_button">
+          &lt;
+        </button>
+    )
+}
