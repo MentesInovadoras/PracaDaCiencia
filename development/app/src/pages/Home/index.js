b83455325d5f9cf1327f61515af06c0ec1b2e61f
Elizeta <luiz.elizeta.123@gmail.com>
2024-10-16 15:48:05 -0300

    feature(PRACA): inicio do desenvolvimento da segunda pagina

---
+++ development/app/src/pages/Home/index.js
@@ -0,0 +1,23 @@
+import './styles.css'
+
+export default function Home(){
+    return(
+        <>
+        <div>
+            <div>
+                <h1 class = "logo">Logo da praça da ciencia</h1>
+            </div>
+            <div class = "blox">
+                <div class = "left">
+                    <h1>Caixa Flutuante Esquerdo</h1>
+                    <h2>Agendamento Online</h2>
+                </div>
+                <div class = "right">
+                    <h1>Caixa flutuante direito</h1>
+                    <h2>Agendamento Online</h2>
+                </div>
+            </div>
+        </div>
+        </>
+    );
+}
