c8eba9c4fefc0b760127ac389cf11cbb5750471a
Elizeta <luiz.elizeta.123@gmail.com>
2024-10-21 16:07:50 -0300

    feature(PRACA): Adição de background, logo e ajustes

--- development/app/src/pages/Home/index.js
+++ development/app/src/pages/Home/index.js
@@ -1,20 +1,23 @@
 import './styles.css'
+import logo_praca from '../images/logo_praca.png'
+import backgroung from '../images/img1.jpg'
 
 export default function Home(){
     return(
         <>
         <div id = "background">
             <div>
-                <h1 class = "logo">Logo da praça da ciencia</h1>
+                <img src={logo_praca} alt="Logo" class = "logo" />
+                {/* <h1 class = "logo">Logo da praça da ciencia</h1> */}
             </div>
             <div class = "box">
                 <div class = "left">
                     <h1 id = "left_box">Visitante</h1>
-                    <h2 class = "buttom">Agendamento Online</h2>
+                    <h2 class = "button">Agendamento Online</h2>
                 </div>
                 <div class = "right">
                     <h1 id = "right_box">Escolas, grupos ou instiuições</h1>
-                    <h2 class = "buttom">Agendamento Online</h2>
+                    <h2 class = "button">Agendamento Online</h2>
                 </div>
             </div>
         </div>
